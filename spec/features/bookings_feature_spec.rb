c964cddf0f1c39bc1c14e3a7507a60d3b14ea6ee
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-28 11:42:45 +0000

    create booking controller and model

--- spec/features/bookings_feature_spec.rb
+++ spec/features/bookings_feature_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 feature 'Bookings' do
+
   context 'no bookings have been added yet' do
     scenario 'should display a prompt to add a booking' do
       visit '/bookings'
       expect(page).to have_content 'No bookings yet'
       expect(page).to have_link 'Create booking'
     end
+  end
+
+
+  context 'a booking has been added' do
+    before do
+      Booking.create(name: 'workshop', description: 'it is hard not to fall asleep', start_time: DateTime.now, end_time: DateTime.now+60)
+    end
+
+    scenario 'should display content of the booking' do
+      visit '/bookings'
+      expect(page).to have_content 'workshop'
+      expect(page).to have_content 'it is hard not to fall asleep'
+    end
+
   end
 end
