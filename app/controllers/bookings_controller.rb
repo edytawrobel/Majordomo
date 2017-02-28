01151b15ad1db1d2cb1c204f451d95e21a10fee9
Stefan Liute <stefanliute@gmail.com>
2017-02-28 17:22:43 +0000

    Create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,6 +8,13 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.create(booking_params)
+    redirect_to bookings_path
+  end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:name, :description, :start_time, :end_time)
   end
 end
