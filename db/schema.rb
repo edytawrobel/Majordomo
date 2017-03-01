2d3276dc49302d78f5363806cb01fe4f5c0c2662
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-03-01 13:22:13 +0000

    add validations on database level

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228113253) do
+ActiveRecord::Schema.define(version: 20170301131651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",        null: false
     t.text     "description"
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.datetime "start_time",  null: false
+    t.datetime "end_time",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
