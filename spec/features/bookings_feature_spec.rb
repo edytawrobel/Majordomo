0a1b4cafe677cee3c5a4239f52c276d4e66f763b
Stefan Liute <stefanliute@gmail.com>
2017-03-02 21:54:58 +0000

    First refactoring

--- spec/features/bookings_feature_spec.rb
+++ spec/features/bookings_feature_spec.rb
@@ -1,92 +1,97 @@
 require 'rails_helper'
 
 feature 'Bookings' do
-  # context 'no bookings have been added yet' do
-  #   scenario 'should display a prompt to add a booking' do
-  #     visit bookings_path
-  #     expect(page).to have_content 'No bookings yet'
-  #     expect(page).to have_link 'Create booking'
-  #   end
-  # end
-
-  context 'creating a new booking' do
+  context 'do not exist in the beginning' do
     let!(:room){ Room.create(name: 'Joy Room') }
 
-    scenario 'should display content of the booking' do
+    scenario 'so users see a message and a link to create a booking' do
+      visit room_path(room)
+      expect(page).to have_content 'No bookings yet!'
+      expect(page).to have_link 'Create booking'
+    end
+  end
+
+  context 'can be created' do
+    let!(:room){ Room.create(name: 'Joy Room') }
+
+    scenario 'via a form, then users can see the current week\'s calendar (with their booking, if in that week),' do
       visit room_path(room)
       click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
       fill_in 'booking_start_time', with: DateTime.now
-      fill_in 'booking_end_time', with: (DateTime.now + 300)
+      fill_in 'booking_end_time', with: (DateTime.now + 5.minutes)
       click_button 'Book'
 
       expect(current_path).to eq room_path(room)
+      expect(page).not_to have_content 'No bookings yet!'
       expect(page).to have_content 'Workshop'
     end
 
-    scenario 'should display validations' do
+    scenario 'and the form has the relevant field validations.' do
       visit new_room_booking_path(room)
       expect(page).to have_xpath("//input[@required='required']")
     end
   end
 
-  context 'viewing a single booking' do
+  context 'have their own individual pages' do
     let!(:room){ Room.create(name: 'Joy Room') }
 
-    scenario 'should display booking info and options' do
+    scenario 'that display specific information and options' do
       visit room_path(room)
       click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
       fill_in 'booking_start_time', with: DateTime.now
-      fill_in 'booking_end_time', with: (DateTime.now + 5)
+      fill_in 'booking_end_time', with: (DateTime.now + 5.minutes)
       click_button 'Book'
       expect(current_path).to eq room_path(room)
-      save_and_open_page
       click_link 'Workshop'
       expect(page).to have_content 'Workshop'
       expect(page).to have_content 'it is hard not to fall asleep'
+      expect(page).to have_content DateTime.now.strftime('%e %B %Y, %H:%M')
+      expect(page).to have_content (DateTime.now + 5.minutes).strftime('%e %B %Y, %H:%M')
       expect(page).to have_link 'Edit'
       expect(page).to have_link 'Cancel'
     end
   end
 
-  context 'editing a booking' do
+  context 'can be edited' do
     let!(:room){ Room.create(name: 'Joy Room') }
 
-    scenario 'should update booking info' do
+    scenario 'via a form' do
       visit room_path(room)
-      click_link 'Book'
+      click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
-      fill_in 'booking_start_time', with: '2017-01-01 17:19:00'
-      fill_in 'booking_end_time', with: '2017-01-01 17:21:00'
+      fill_in 'booking_start_time', with: DateTime.now
+      fill_in 'booking_end_time', with: (DateTime.now + 5.minutes)
       click_button 'Book'
       expect(current_path).to eq room_path(room)
       click_link 'Workshop'
       click_link 'Edit'
-      fill_in 'Description', with: 'Has a much shorter description now'
-      fill_in 'booking_start_time', with: '2017-02-01 16:00:00'
-      fill_in 'booking_end_time', with: '2017-02-01 16:10:00'
+      fill_in 'Name', with: 'Much tinier workshop'
+      fill_in 'Description', with: 'so no sleeping'
+      fill_in 'booking_start_time', with: (DateTime.now + 10.minutes)
+      fill_in 'booking_end_time', with: (DateTime.now + 15.minutes)
       click_button 'Update'
       expect(current_path).to eq room_path(room)
       expect(page).to have_content 'Booking updated!'
-      expect(page).to have_content 'Has a much shorter description now'
-      expect(page).to have_content '1 February 2017, 16:00'
+      expect(page).to have_content 'Much tinier workshop'
+      expect(page).to have_content (DateTime.now + 10.minutes).strftime('%H:%M')
     end
   end
 
-  context 'cancelling a booking' do
+  context 'can be cancelled' do
     let!(:room){ Room.create(name: 'Joy Room') }
 
-    scenario 'should destroy that booking' do
+    scenario 'via a link' do
       visit room_path(room)
-      click_link 'Book'
+      click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
-      fill_in 'booking_start_time', with: '2017-01-01 17:19:00'
-      fill_in 'booking_end_time', with: '2017-01-01 17:21:00'
+      fill_in 'booking_start_time', with: DateTime.now
+      fill_in 'booking_end_time', with: (DateTime.now + 5.minutes)
       click_button 'Book'
       expect(current_path).to eq room_path(room)
       click_link 'Workshop'
@@ -94,7 +99,7 @@ feature 'Bookings' do
       expect(current_path).to eq room_path(room)
       expect(page).to have_content 'Booking cancelled!'
       expect(page).not_to have_content 'Workshop'
-      expect(page).not_to have_content '1 January 2017, 17:19'
+      expect(page).not_to have_content (DateTime.now + 5.minutes).strftime('%H:%M')
     end
   end
 end
--- spec/features/view_status_spec.rb
+++ spec/features/view_status_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-feature 'View status' do
-  context 'when a booking is in progress' do
+feature 'Users can view a room status' do
+  context 'and when a booking is in progress' do
     let!(:room){ Room.create(name: 'Joy Room') }
     # let!(:booking){ Booking.create(name: 'First booking', description: 'Has a long description here', start_time: DateTime.now-300, end_time: DateTime.now+300, room_id: 1) }
-    scenario 'status is busy' do
+    scenario 'the status is "in use"' do
       visit room_path(room)
-      click_link 'Book'
+      click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
       fill_in 'booking_start_time', with: DateTime.now
-      fill_in 'booking_end_time', with: (DateTime.now + 60)
+      fill_in 'booking_end_time', with: (DateTime.now + 5.minutes)
       click_button 'Book'
       visit status_room_bookings_path(room)
       expect(page).to have_content 'Room in use'
     end
   end
 
-  context 'when there is no ongoing booking' do
+  context 'and when no booking is ongoing' do
     let!(:room){ Room.create(name: 'Joy Room') }
-    scenario 'status is free' do
+    scenario 'the status is "available"' do
       visit room_path(room)
-      click_link 'Book'
+      click_link 'Create booking'
       fill_in 'Name', with: 'Workshop'
       fill_in 'Description', with: 'it is hard not to fall asleep'
-      fill_in 'booking_start_time', with: (DateTime.now - 600)
-      fill_in 'booking_end_time', with: ( DateTime.now - 300)
+      fill_in 'booking_start_time', with: (DateTime.now - 10.minutes)
+      fill_in 'booking_end_time', with: ( DateTime.now - 5.minutes)
       click_button 'Book'
       visit status_room_bookings_path(room)
       expect(page).to have_content 'Room available'
