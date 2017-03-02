53ccfd48373e43f33718333270bb57c36391cb93
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-03-02 11:19:25 +0000

    room validations for name and uniqueness

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+  validates :name, uniqueness: true, presence: true, length: { minimum: 4, maximum: 16}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302105512) do
+ActiveRecord::Schema.define(version: 20170302111251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20170302105512) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_rooms_on_name", unique: true, using: :btree
   end
 
 end
