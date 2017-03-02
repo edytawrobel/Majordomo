72d74a24259d1d880d6241eb8569af2fbf36f812
Shoniwa Marovatsanga <stmarovatsanga@gmail.com>
2017-03-02 11:57:30 +0000

    fixed test

--- spec/features/view_status_spec.rb
+++ spec/features/view_status_spec.rb
@@ -5,7 +5,7 @@ feature 'View status' do
     let!(:booking){ Booking.create(name: 'Single booking', description: 'Has a long description here', start_time: DateTime.now-300, end_time: DateTime.now+300) }
     scenario 'status is busy' do
       visit 'bookings/status'
-      expect(page).to have_content 'Busy'
+      expect(page).to have_content 'Room in use'
     end
   end
 
@@ -13,7 +13,7 @@ feature 'View status' do
     let!(:booking){ Booking.create(name: 'Single booking', description: 'Has a long description here', start_time: DateTime.now-600, end_time: DateTime.now-60) }
     scenario 'status is free' do
       visit 'bookings/status'
-      expect(page).to have_content 'Free'
+      expect(page).to have_content 'Room available'
     end
   end
 end
